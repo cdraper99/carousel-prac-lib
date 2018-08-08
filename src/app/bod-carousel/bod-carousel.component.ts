555b1acb7acea757413a9a401d31a1f1de5b1bca
Charlie Draper <charlie.draper@soprasteria.com>
2018-08-08 19:02:17 +0100

    beginning of next and preivous impl

--- src/app/bod-carousel/bod-carousel.component.ts
+++ src/app/bod-carousel/bod-carousel.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-bod-carousel',
@@ -13,7 +13,7 @@ export class BodCarouselComponent implements OnInit {
   // 5 items to be displayed
   displayedItems: Array<any>;
   // index to display items by
-  index: number;
+  index = 0;
 
   constructor() { }
 
@@ -21,7 +21,40 @@ export class BodCarouselComponent implements OnInit {
     this.displayedItems = this.itemArray.slice(0, 5);
   }
 
-  updateDisplayedItems(index, array) {
+  getDistanceFromEnd(index, array) {
+    const remainder = index % array.length;
+    const imageIndex = Math.floor(remainder >= 0 ? remainder : remainder + array.length);
+
+    return array[imageIndex];
+  }
+
+  updateCarousel(array: Array<any>, index) {
+    return array.slice(index, index + 5);
+  }
+
+  next() {
+    this.index++;
+    // this.displayedItems = this.getDistanceFromEnd(this.index, this.itemArray);
+    console.log(this.index);
+    console.log(this.getDistanceFromEnd(this.index, this.itemArray));
+    console.log(this.displayedItems);
+    console.log(this.itemArray);
+    
+    if (this.displayedItems)
+
+    this.displayedItems = this.updateCarousel(this.itemArray, this.index);
+
+  }
+
+  previous() {
+    this.index--;
+    // this.displayedItems = this.getDistanceFromEnd(this.index, this.itemArray);
+    console.log(this.index);
+    console.log(this.getDistanceFromEnd(this.index, this.itemArray));
+    console.log(this.displayedItems);
+    console.log(this.itemArray);
+    
+    this.displayedItems = this.updateCarousel(this.itemArray, this.index);
 
   }
 
